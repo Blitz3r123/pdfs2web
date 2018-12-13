705bdb16f4a1115822c554ce3063ef097eb84fdc
Kaleem <KaleemPeeroo@gmail.com>
2018-12-13 16:39:03 +0000

    Fixed a quick bug

--- files/test.bat
+++
@@ -1,6 +0,0 @@
-call pdftohtml.exe DSA3_IN2002_2018.pdf DSA3_IN2002_2018.html
-call pdftohtml.exe DSA4_IN2002_2018.pdf DSA4_IN2002_2018.html
-call pdftohtml.exe DSA5_IN2002_2018.pdf DSA5_IN2002_2018.html
-call pdftohtml.exe DSA6_IN2002_2018.pdf DSA6_IN2002_2018.html
-call pdftohtml.exe DSA7_IN2002_2018.pdf DSA7_IN2002_2018.html
-call dir
